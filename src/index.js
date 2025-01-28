02ae4672dca9f3adfd6846edabfd10c986f3275d
Anil Yamangil <anil.y@blue.cloud>
2025-01-28 22:43:02 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css'; // Optional: Include your global styles
-import App from './App';
+import App from './app'; // <-- Change 'App' to 'app' here
 
+// ... existing code ...
 ReactDOM.render(
   <React.StrictMode>
     <App />
